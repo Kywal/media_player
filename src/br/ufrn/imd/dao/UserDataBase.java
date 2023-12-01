15d48e1fd073f26133c6ccc9d052f9e44f67d169
Emanuel Kywal <emanuelkywal@outlook.com>
2023-12-01 16:06:52 -0300

    feat: add singleton implemenation to UserDataBase and declare other methods

--- src/br/ufrn/imd/dao/UserDataBase.java
+++ src/br/ufrn/imd/dao/UserDataBase.java
@@ -1,5 +1,77 @@
 package br.ufrn.imd.dao;
 
-public class Repository {
+import java.util.Vector;
 
+import br.ufrn.imd.model.User;
+
+public class UserDataBase {
+
+	private static UserDataBase instance;
+	protected Vector<User> users;
+
+
+// -------------- Singleton implementation -------------- //
+	
+	/**
+	 * 
+	 */
+	private UserDataBase() {
+		users = new Vector<User>();
+		users = getUsersFromDB();
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static UserDataBase getInstance() {
+		if(instance == null) {
+			instance = new UserDataBase();
+		}
+		
+		return instance;
+	}
+	
+// ------------------ Start/End DB methods ------------------------ //
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private Vector<User> getUsersFromDB() {
+		// Read users from users.txt
+		return users;
+	}
+	
+//	---------------- Utilitary User methods ----------------------- //
+	
+	/**
+	 * 
+	 * @param user
+	 */
+	public void signUpUser(User user) {
+		
+	}
+	
+// ----------------- I/O from DB methods -------------------------- //
+	
+	/**
+	 * 
+	 * @param user
+	 */
+	public void writeUser(User user) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param filePath
+	 * @return
+	 */
+	public Vector<String> readUser(String filePath) {
+		Vector<String> file = new Vector<String>();
+		
+		return file;
+	}
+	
 }
