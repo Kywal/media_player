3765de9488e4e1f17c4a9ab45dd28e6e9bd0d105
Emanuel Kywal <emanuelkywal@outlook.com>
2023-12-02 02:28:17 -0300

    feat: add implementation of singleton in PlaylistDataBase

--- src/br/ufrn/imd/dao/PlaylistDataBase.java
+++ src/br/ufrn/imd/dao/PlaylistDataBase.java
@@ -1,5 +1,37 @@
 package br.ufrn.imd.dao;
 
+import java.util.Vector;
+import br.ufrn.imd.model.Playlist;
+
 public class PlaylistDataBase {
 
+	private static PlaylistDataBase instance;
+	protected Vector<Playlist> playlists;
+
+	/**
+	 * 
+	 */
+	private PlaylistDataBase() {
+		playlists = new Vector<Playlist>();
+		getPlaylistsFromDB();
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public static PlaylistDataBase getInstance() {
+		if(instance == null) {
+			instance = new PlaylistDataBase();
+		}
+	     
+		return instance;
+	}
+
+	/**
+	 * 
+	 */
+	private void getPlaylistsFromDB() {
+	       
+	}
 }
