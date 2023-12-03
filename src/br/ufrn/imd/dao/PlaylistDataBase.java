1c038540208208459d555ca92312567f497e5dc5
Emanuel Kywal <emanuelkywal@outlook.com>
2023-12-03 12:50:44 -0500

    feat: add and test playlist write in DB

--- src/br/ufrn/imd/dao/PlaylistDataBase.java
+++ src/br/ufrn/imd/dao/PlaylistDataBase.java
@@ -1,15 +1,22 @@
 package br.ufrn.imd.dao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.Vector;
+
+import br.ufrn.imd.model.Music;
 import br.ufrn.imd.model.Playlist;
 import br.ufrn.imd.model.User;
+import br.ufrn.imd.model.VipUser;
 
 public class PlaylistDataBase {
 
 	private static PlaylistDataBase instance;
-	private String filePath = "database\\playlists"; // TODO -> Remove when dinamic filePath is implemented
+	private String filePath = "database\\playlists\\"; // TODO -> Remove when dinamic filePath is implemented
 	protected Vector<Playlist> playlists;
 
 // -------------- Singleton implementation -------------- //	
@@ -40,7 +47,9 @@ public class PlaylistDataBase {
 	 * 
 	 */
 	private void getPlaylistsFromDB() {
-	       
+//		Vector<Vector<Vector<String>>> files = readPlaylists();
+		
+		
 	}
 	
 //	---------------- CRUD Playlist methods ----------------------- //
@@ -48,9 +57,28 @@ public class PlaylistDataBase {
 	/**
 	 * 
 	 * @param p
+	 * @throws IOException 
 	 */
-	public void addPlaylist(User user, Playlist p) {
-//		user.set
+	public void addPlaylist(VipUser user, Playlist p) throws IOException {
+		try {
+
+			if(playlists.isEmpty()) {
+				p.setPlaylistID(1111);
+				playlists.add(p);
+				((VipUser)user).setPlaylists(playlists);
+				writePlaylist(user, p);
+			} else {
+				p.setPlaylistID(playlists.lastElement().getPlaylistID() + 1);
+				playlists.add(p);
+				((VipUser)user).setPlaylists(playlists);
+				writePlaylist(user, p);
+			}				
+			
+		} catch (IOException e) {
+			System.out.println("An error ocurred while adding the new Playlist");
+			e.printStackTrace();
+		}
+		
 		
 	}
 
@@ -62,11 +90,43 @@ public class PlaylistDataBase {
 	 * @throws IOException 
 	 */
 	private void writePlaylist(User user, Playlist p) throws IOException {
+		String fileName = "playlist_" + p.getPlaylistID() + ".txt";
+		String dash = "-";
 		
 		try {
-			FileWriter myWriter = new FileWriter(filePath, true);	
-//			myWriter.write("")
+			FileWriter myWriter = new FileWriter(filePath + fileName);	
+			myWriter.write("UserID-" + user.getUserID() + "-Password-" + user.getPassword() + "\n" +
+							p.getPlaylistID() + dash +
+							p.getName() + dash + 
+							p.getSubTitle() + dash + 
+							p.getCreationDate() + dash +
+							p.getDuration() + dash + 
+							"\n"
+							);
 			
+			for (Music music : p.getMusics()) {
+				myWriter.write("music" + dash + 
+								music.getName() + dash +
+								music.getPath() + dash +
+								music.getLanguage() + dash +
+								music.getYear() + dash +
+								music.getDuration() + dash
+								);
+				
+				myWriter.write("author");
+				for (String author : music.getAuthors()) {
+					myWriter.write(dash + author);
+				}
+				
+				
+				myWriter.write(dash + "genre");
+				for (String genre : music.getGenres()) {
+					myWriter.write(dash + genre);
+				}
+				myWriter.write("\n");
+			}
+			
+			myWriter.close();
 		} catch (IOException e) {
 			
 		}
@@ -78,8 +138,57 @@ public class PlaylistDataBase {
 	 * 
 	 * @return
 	 */
-	private Vector<Vector<String>> readPlaylist() {
-		return new Vector<Vector<String>>(); // STUB 
+	protected Vector<Vector<Vector<String>>> readPlaylists() {
+		Reader reader = null;
+		String data = "";
+		Vector<String> line = new Vector<String>();
+		Vector<Vector<String>> file = new Vector<Vector<String>>();
+		Vector<Vector<Vector<String>>> filesMatrix = new Vector<Vector<Vector<String>>>();
+		 
+		File playlistFolder = new File(filePath);
+		File files[] = playlistFolder.listFiles();
+		
+		for (int i = 0; i < files.length; i++) {
+			String fileName = files[i].getName();
+			
+			try {
+				
+				int num = 0;
+				reader = new FileReader(filePath + fileName);
+				
+				while((num = reader.read()) != -1) {
+					char character = (char) num;
+					
+					if(character == '\n') {
+						line.add(data);
+						data = "";
+						
+						@SuppressWarnings("unchecked")
+						Vector<String> newLine = (Vector<String>) line.clone();
+						file.add(newLine);
+						line.clear();
+					} 
+					else if(character == '-') {
+						line.add(data);
+						data = "";
+					} else {
+						data += character;															
+					}
+				}
+				
+				filesMatrix.add(file);
+				
+				
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+		}
+		
+		
+		return filesMatrix;
 	}
 	
 }
--- src/br/ufrn/imd/dao/Test.java
+++ src/br/ufrn/imd/dao/Test.java
@@ -1,8 +1,11 @@
 package br.ufrn.imd.dao;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Vector;
 
+import br.ufrn.imd.model.Music;
+import br.ufrn.imd.model.Playlist;
 import br.ufrn.imd.model.User;
 import br.ufrn.imd.model.VipUser;
 
@@ -10,32 +13,71 @@ public class Test {
 
 	public static void main(String[] args) throws IOException {
 		
-		UserDataBase db = UserDataBase.getInstance();
+		PlaylistDataBase db = PlaylistDataBase.getInstance();
 		
 		VipUser vipUser = new VipUser();
+		vipUser.setUsername("Emanuel Kywal");
+		vipUser.setUserID(1);
 		vipUser.setEmail("emanuelkywal@outlook.com");
 		vipUser.updatePassword("12345");
-		vipUser.setUsername("Emanuel Kywal");
 		
-		System.out.println(vipUser instanceof User);
+		Playlist p1 = new Playlist();
+		p1.setName("Memento");
+		p1.setSubTitle("Lofi and others calm songs");
+		p1.setDuration(56.4);
+
+		Music m1 = new Music();
+		m1.setPath("teste\\musiquitas\\fifteen_minutes.wavv");
+		m1.setName("Fifteen Minutes");
+		m1.setYear(2009);
+		m1.setLanguage("Ingles");
+		m1.setDuration(3.6);
 		
-//		User cUser = new User();
-//		cUser.setEmail("kywalfilho@outlook.com");
-//		cUser.updatePassword("98765");
-//		cUser.setUsername("Kywal filho");
-//		
-//		db.signUpUser(vipUser);
-//		db.signUpUser(cUser);
-//		
-//		db = UserDataBase.getInstance();
-//		
-//		for (User user : db.users) {
-//			System.out.println(user.getEmail());
-//			System.out.println(user.getPassword());
-//			System.out.println(user.getUsername());
-//			System.out.println(user.getIsVip());
-//			System.out.println(user.getUserID());
-//		}
+		ArrayList<String> genre1 = new ArrayList<String>();
+		genre1.add("Garage Rock");
+		m1.setGenres(genre1);
+		
+		ArrayList<String> authors1 = new ArrayList<String>();
+		authors1.add("Mike Krol");
+		m1.setAuthors(authors1);
+		
+		Music m2 = new Music();
+		m2.setPath("teste\\musiquitas\\fifteen_minutes.wavv");
+		m2.setName("Fifteen Minutes");
+		m2.setYear(2009);
+		m2.setLanguage("Ingles");
+		m2.setDuration(3.6);
+		
+		ArrayList<String> genre2 = new ArrayList<String>();
+		genre2.add("Garage Rock");
+		m2.setGenres(genre2);
+		
+		ArrayList<String> authors2 = new ArrayList<String>();
+		authors2.add("Mike Krol");
+		m2.setAuthors(authors2);
+		
+		Music m3 = new Music();
+		m3.setPath("teste\\musiquitas\\fifteen_minutes.wavv");
+		m3.setName("Fifteen Minutes");
+		m3.setYear(2009);
+		m3.setLanguage("Ingles");
+		m3.setDuration(3.6);
+		
+		ArrayList<String> genre3 = new ArrayList<String>();
+		genre3.add("Garage Rock");
+		m3.setGenres(genre3);
+		
+		ArrayList<String> authors3 = new ArrayList<String>();
+		authors3.add("Mike Krol");
+		m3.setAuthors(authors3);
+		
+		p1.addMusic(m1);
+		p1.addMusic(m2);
+		p1.addMusic(m3);
+		
+		db.addPlaylist(vipUser, p1);
+		
+		
 	}
 
 }
